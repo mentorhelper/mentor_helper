ee185482ba43d249cf3dc054c1ee9a3fadecc5b7
akupreychik <anton.kupreychik@hf-solution.com>
2022-09-30 01:19:18 +0300

    MH-13: change property and add correct bot creating

--- src/main/java/com/ua/javaruh/mentor/bot/MentorHelperBot.java
+++ src/main/java/com/ua/javaruh/mentor/bot/MentorHelperBot.java
@@ -1,27 +1,49 @@
 package com.ua.javaruh.mentor.bot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+@Slf4j
+@Configuration
 public class MentorHelperBot extends TelegramLongPollingBot {
     private static final String GREETINGS = "Welcome to MentorHelper";
-    @Value("${telegramBot.token}")
-    private String token;
 
-    @Value("${telegramBot.username}")
-    private String username;
+    private String botUsername;
+    private String botToken;
+
+    @Bean
+    public MentorHelperBot uploadBot(@Value("${telegramBot.username}") String botUsername,
+                                     @Value("${telegramBot.token}") String botToken) {
+        this.botUsername = botUsername;
+        this.botToken = botToken;
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(this);
+            log.info("Bot {} was registered", botUsername);
+        } catch (TelegramApiException e) {
+            log.error("Error while registering bot", e);
+        }
+        return this;
+    }
+
 
     @Override
     public String getBotToken() {
-        return token;
+        return botToken;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+            log.info("Message '{}' from user with id {}", update.getMessage().getText(), update.getMessage().getFrom().getId());
             SendMessage message = new SendMessage();
             message.setChatId(update.getMessage().getChatId());
 
@@ -40,6 +62,6 @@ public class MentorHelperBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return username;
+        return botUsername;
     }
 }
--- src/main/java/com/ua/javaruh/mentor/controller/RoleController.java
+++ src/main/java/com/ua/javaruh/mentor/controller/RoleController.java
@@ -13,37 +13,37 @@ public class RoleController {
     }
 
     @GetMapping("/{roleId}")
-    public Role getRoleName() {
+    public Role getRoleName(@PathVariable Long roleId) {
         //find role by id
         return new Role(1, "getRoleName");
     }
 
     @GetMapping("/{roleId}/permission")
-    public Role getRolePermission() {
+    public Role getRolePermission(@PathVariable Long roleId) {
         //find role permission by id and return
         return new Role(2, "getRolePermission");
     }
 
-    @PostMapping("/{roleId}")
+    @PostMapping("")
     public Role createNewRole() {
         //Create new role
         return new Role(3, "createNewRole");
     }
 
     @PostMapping("/{roleId}/permission")
-    public Role addPermissionToRole() {
+    public Role addPermissionToRole(@PathVariable Long roleId) {
         //Create new permission for role
         return new Role(4, "addPermission");
     }
 
     @DeleteMapping("/{roleId}")
-    public Role removeRole() {
+    public Role removeRole(@PathVariable Long roleId) {
         //remove role
         return new Role(5, "removeRole");
     }
 
     @DeleteMapping("/{roleId}/permission")
-    public Role removePermissionInRole() {
+    public Role removePermissionInRole(@PathVariable Long roleId) {
         //remove permission in role
         return new Role(6, "removePermissionInRole");
     }
