bb4f0dea984a63add9e331f525a22d7dc374b55e
akupreychik <anton.kupreychik@hf-solution.com>
2022-10-07 01:03:17 +0300

    MA-10: edit data and fix names

--- src/main/java/com/ua/javarush/mentor/persist/repository/ModuleRepository.java
+++ src/main/java/com/ua/javarush/mentor/persist/repository/ModuleRepository.java
@@ -4,5 +4,6 @@ package com.ua.javarush.mentor.persist.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 }
--- src/main/java/com/ua/javarush/mentor/persist/repository/TimeTableRepository.java
+++ src/main/java/com/ua/javarush/mentor/persist/repository/TimeTableRepository.java
@@ -2,6 +2,8 @@ package com.ua.javarush.mentor.persist.repository;
 
 import com.ua.javarush.mentor.persist.model.TimeTable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TimeTableRepository extends JpaRepository<TimeTable, Long> {
 }
